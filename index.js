d982b0739660f8a8b8d89337b7b501be13feb6b4
Kimly <kimlyvithkimly@gmail.com>
2021-10-15 20:53:28 +0700

    add new feature

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-	res.send("This is app2 - webhook by github notify to stack channel. ");
+	res.send("<h3>Testing notification slack channel deployment</h3>");
 });
 
 app.listen(8080,function(){
